dda68859a3cdb66b3803f0926b503064387a8c2d
陈铭鹤 <615880048@qq.com>
2024-07-04 10:52:59 +0800

    重新拷入代码

--- .vuepress/sidebar.ts
+++ .vuepress/sidebar.ts
@@ -4,6 +4,6 @@ import roadmapSideBar from "./sidebars/roadmapSideBar";
 // @ts-ignore
 export default {
     "/学习路线/": roadmapSideBar,
-    // 降级，默认根据文章的标题渲染侧边栏
+    // 降级，默认根据文章标题渲染侧边栏
     "/": "auto",
 } as SidebarConfig4Multiple;
